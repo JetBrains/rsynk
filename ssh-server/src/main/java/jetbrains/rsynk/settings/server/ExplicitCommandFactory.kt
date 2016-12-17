7f3f6e962936996af7dffea4562f084e0e4cf605
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2016-12-18 01:14:10 +0300

    Delete auto-generated comments

--- ssh-server/src/main/java/jetbrains/rsynk/settings/server/ExplicitCommandFactory.kt
+++ ssh-server/src/main/java/jetbrains/rsynk/settings/server/ExplicitCommandFactory.kt
@@ -4,7 +4,7 @@ import org.apache.sshd.server.Command
 import org.apache.sshd.server.CommandFactory
 
 class ExplicitCommandFactory : CommandFactory {
-  override fun createCommand(command: String?): Command {
-    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+  override fun createCommand(command: String): Command {
+    throw UnsupportedOperationException("not implemented")
   }
 }
--- ssh-server/src/main/java/jetbrains/rsynk/settings/server/SSHSessionFactory.kt
+++ ssh-server/src/main/java/jetbrains/rsynk/settings/server/SSHSessionFactory.kt
@@ -5,10 +5,10 @@ import org.apache.sshd.server.session.SessionFactory
 
 class SSHSessionFactory {
   fun createSessionFactory(): SessionFactory {
-    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    throw UnsupportedOperationException("not implemented")
   }
 
   fun createSessionListener(): SessionListener {
-    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    throw UnsupportedOperationException("not implemented")
   }
 }
