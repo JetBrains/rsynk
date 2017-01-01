04acac89cd12b91a4a6a337a3aa881e7382b317a
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2017-01-01 22:49:27 +0300

    Add file deletion test

--- server/src/test/java/jetbrains/rsynk/server/IntegrationTest.kt
+++ server/src/test/java/jetbrains/rsynk/server/IntegrationTest.kt
@@ -6,11 +6,14 @@ import org.junit.BeforeClass
 import org.junit.Test
 import java.io.File
 import java.nio.file.Files
+import java.util.concurrent.atomic.AtomicInteger
 
 class IntegrationTest {
   companion object {
     @JvmStatic
     val rsynk = Rsynk(20333/*TODO: get available port*/, 1, 1, 100, TestTools.getServerKey())
+
+    val password = "letmein"
 
     @BeforeClass
     @JvmStatic
@@ -19,42 +22,55 @@ class IntegrationTest {
     @BeforeClass
     @JvmStatic
     fun stopServer() = rsynk.stop()
+
+    val id = AtomicInteger(0)
   }
 
   @Test
   fun file_transfer_test() {
-    val dataDir = Files.createTempDirectory("data-1").toFile()
+    val dataDir = Files.createTempDirectory("data-${id.incrementAndGet()}").toFile()
     val source = File(dataDir, "from.txt")
     source.writeText(TestTools.loremIpsum)
     val destination = File(dataDir, "to.txt")
-    Rsync.syncFile("rsync://localhost:${source.absolutePath}", destination.absolutePath, rsynk.port, "letmein", 10)
+    Rsync.syncFile("rsync://localhost:${source.absolutePath}", destination.absolutePath, rsynk.port, password, 10)
     Assert.assertEquals(TestTools.loremIpsum, destination.readText())
   }
 
   @Test
   fun incremental_file_transfer_test() {
-    val dataDir = Files.createTempDirectory("data-2").toFile()
+    val dataDir = Files.createTempDirectory("data-${id.incrementAndGet()}").toFile()
     val source = File(dataDir, "from.txt")
     source.writeText(TestTools.loremIpsum)
     val destination = File(dataDir, "to.txt")
     destination.writeText(TestTools.loremIpsum.substring(0, TestTools.loremIpsum.length / 2))
-    Rsync.syncFile("rsync://localhost:${source.absolutePath}", destination.absolutePath, rsynk.port, "letmein", 10)
+    Rsync.syncFile("rsync://localhost:${source.absolutePath}", destination.absolutePath, rsynk.port, password, 10)
     Assert.assertEquals(TestTools.loremIpsum, destination.readText())
   }
 
   @Test
   fun directory_transfer_test() {
-    val sourceDir = Files.createTempDirectory("data-3").toFile()
+    val sourceDir = Files.createTempDirectory("data-${id.incrementAndGet()}").toFile()
     val firstSource = File(sourceDir, "first.txt")
     val secondSource = File(sourceDir, "second.txt")
     firstSource.writeText(TestTools.loremIpsum)
     secondSource.writeText(TestTools.loremIpsum)
-    val destinationDir = Files.createTempDirectory("data-4").toFile()
-    Rsync.syncDirectory("rsync://localhost:${sourceDir.absolutePath}", destinationDir.absolutePath, rsynk.port, "letmein", 10)
+    val destinationDir = Files.createTempDirectory("data-${id.incrementAndGet()}").toFile()
+    Rsync.syncDirectory("rsync://localhost:${sourceDir.absolutePath}", destinationDir.absolutePath, rsynk.port, password, 10)
     Assert.assertTrue(destinationDir.listFiles().any { it.name == firstSource.name })
     Assert.assertTrue(destinationDir.listFiles().any { it.name == secondSource.name })
     destinationDir.listFiles().forEach { file ->
       Assert.assertEquals(TestTools.loremIpsum, file.readText())
     }
+  }
+
+  @Test
+  fun directory_file_deletion_test() {
+    val sourceDir = Files.createTempDirectory("data-${id.incrementAndGet()}").toFile()
+    File(sourceDir, "first.txt").writeText(TestTools.loremIpsum)
+    val destinationDir = Files.createTempDirectory("data-${id.incrementAndGet()}").toFile()
+    File(destinationDir, "first.txt").writeText(TestTools.loremIpsum)
+    File(destinationDir, "second.txt").writeText(TestTools.loremIpsum)
+    Rsync.syncDirectory("rsync://localhost:${sourceDir.absolutePath}", destinationDir.absolutePath, rsynk.port, password, 10)
+    Assert.assertFalse(destinationDir.listFiles().any { it.name == "second.txt" })
   }
 }
