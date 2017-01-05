bbff5f345aa0291150773d976719fd4339701490
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2017-01-05 16:01:53 +0300

    Add new exception

--- rsync/src/main/java/jetbrains/rsynk/errors/Exceptions.kt
+++ rsync/src/main/java/jetbrains/rsynk/errors/Exceptions.kt
@@ -3,4 +3,5 @@ package jetbrains.rsynk.errors
 open class RsynkException(message: String) : RuntimeException(message)
 
 class ProtocolException(message: String) : RsynkException(message)
+class UnsupportedProtocolException(message: String) : RuntimeException(message)
 class ModuleNotFoundException(message: String) : RsynkException(message)
