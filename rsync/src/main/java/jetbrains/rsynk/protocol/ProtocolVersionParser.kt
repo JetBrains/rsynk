2f138fa03e5a61c59984fef79d353242baef61ca
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2017-01-14 18:22:57 +0300

    Parse and keep protocol subversion

--- rsync/src/main/java/jetbrains/rsynk/protocol/ProtocolVersionParser.kt
+++ rsync/src/main/java/jetbrains/rsynk/protocol/ProtocolVersionParser.kt
@@ -5,36 +5,43 @@ import jetbrains.rsynk.exitvalues.UnsupportedProtocolException
 
 class ProtocolVersionParser(protocol: String) {
 
-  val response: String
-  val version: Int
+  val rsyncFormattedProtocolVersion: String
+  val protocolVersion: Int
+  val subProtocolVersion: Int
 
   init {
     if (!protocol.startsWith(Constants.RSYNCD)) {
       throw ProtocolException("Expected ${Constants.RSYNCD}<version>, but was $protocol")
     }
     val clientVersion = protocol.substring(Constants.RSYNCD.length)
-    version = try {
+    val version = try {
+      if (clientVersion.count { it == '.' } > 1) {
+        throw ProtocolException("")
+      }
       if (clientVersion.contains(".")) {
-        clientVersion.split('.')[0].toInt()
+        val aPair = clientVersion.split('.')
+        Pair(aPair[0].toInt(), aPair[1].toInt())
       } else {
-        clientVersion.toInt()
+        Pair(clientVersion.toInt(), 0)
       }
     } catch (t: Throwable) {
       throw ProtocolException("Cannot parse version \'$clientVersion\'")
     }
+    protocolVersion = version.first
+    subProtocolVersion = version.second
     /**
      * Protocols prior to 30 only output <version> alone.  The daemon expects
      * to see a similar greeting back from the client.  For protocols prior to
      * 30, an absent ".<subprotocol>" value is assumed to be 0.  For protocol
      * 30, an absent value is a fatal error.
      */
-    if (version < 30) {
-      response = Constants.RSYNCD + "29" + "\n"
+    if (protocolVersion < 30) {
+      rsyncFormattedProtocolVersion = Constants.RSYNCD + "29" + "\n"
     } else {
-      response = Constants.RSYNCD + "30.0" + "\n"
+      rsyncFormattedProtocolVersion = Constants.RSYNCD + "30.0" + "\n"
     }
-    if (version < 26) {
-      throw UnsupportedProtocolException("Protocols older than 26 (client has $version) is not supported")
+    if (protocolVersion < 26) {
+      throw UnsupportedProtocolException("Protocols older than 26 (client has $protocolVersion) is not supported")
     }
   }
 }
