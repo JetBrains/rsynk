c4f4a4e67aa4695917b6fdff4ecd7144091e90e6
Sergey Voytovich <voytovich.sergeey@gmail.com>
2017-01-28 19:26:19 +0300

    Delete C-like file structure and keep only objects we really need

--- rsync/src/main/java/jetbrains/rsynk/files/FileStructure.kt
+++ rsync/src/main/java/jetbrains/rsynk/files/FileStructure.kt
@@ -2,9 +2,14 @@ package jetbrains.rsynk.files
 
 import java.io.File
 
-class FileStructure (val directory: File,    /* The dir info inside the transfer */
-                     val lastModified: Long, /* When the item was last modified */
-                     val length32: Long,     /* File size % 2 ^ 32 */
-                     val mode: Int,          /* The item's type and permissions */
-                     val flags: Int,         /* The FLAG_* bits for this item */
-                     val file: File)         /* The basename (AKA filename) follows */
+data class FileStructure(val mode: Int,   /* The item's type and permissions */
+                         val flags: Int,  /* The FLAG_* bits for this item */
+                         val file: File) {
+  fun mode(newMode: Int): FileStructure {
+    return FileStructure(newMode, flags, file)
+  }
+
+  fun flags(newFlags: Int): FileStructure {
+    return FileStructure(mode, newFlags, file)
+  }
+}
