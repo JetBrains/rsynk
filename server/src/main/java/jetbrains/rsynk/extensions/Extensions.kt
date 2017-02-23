0a814d332591065e78c2c60105d60e84fe3e06ad
Sergey Voytovich <voytovich.sergeey@gmail.com>
2017-02-23 20:57:48 +0300

    Fix int->bytes and bytes->int casting

--- server/src/main/java/jetbrains/rsynk/extensions/Extensions.kt
+++ server/src/main/java/jetbrains/rsynk/extensions/Extensions.kt
@@ -3,11 +3,24 @@ package jetbrains.rsynk.extensions
 import java.nio.ByteBuffer
 
 
-fun Int.toByteArray(): ByteArray = ByteBuffer.allocate(4).putInt(this).array()
+/**
+ * Rsync transforms bytes to int like this:
+ * {@code
+ *      union {
+ *	  	    const uchar *b;
+ *	    	const uint32 *num;
+ *	    } u;
+ * }
+ * Since bytes for int read from the end to begin.
+ * Reversed array should be sent, pretending rsynk
+ * casts bytes to int same way
+ */
+fun Int.toReversedByteArray(): ByteArray = ByteBuffer.allocate(4).putInt(this).array().reversedArray()
 
-fun ByteArray.toInt(): Int {
+
+fun ByteArray.reverseAndCastToInt(): Int {
   if (this.size != 4) {
     throw IllegalArgumentException("Cannot convert array of ${this.size} to Int")
   }
-  return ByteBuffer.wrap(this).int
+  return ByteBuffer.wrap(this.reversedArray()).int
 }
