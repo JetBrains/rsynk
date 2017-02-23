d5a017cf986e995e3b35f012ffe1afb8a57d0667
Sergey Voytovich <voytovich.sergeey@gmail.com>
2017-02-23 20:37:15 +0300

    Get rid of unused methods

--- server/src/main/java/jetbrains/rsynk/extensions/Extensions.kt
+++ server/src/main/java/jetbrains/rsynk/extensions/Extensions.kt
@@ -2,22 +2,6 @@ package jetbrains.rsynk.extensions
 
 import java.nio.ByteBuffer
 
-fun String.dropNewLine(): String {
-  if (!endsWith('\n')) {
-    throw Error("String not ends with new line symbol: $this")
-  }
-  return dropLast(1)
-}
-
-
-fun String.endsWithNullTerminal(): Boolean = endsWith('\u0000')
-
-fun String.dropNullTerminal(): String {
-  if (!endsWithNullTerminal()) {
-    throw Error("String not ends with null terminal symbol: $this")
-  }
-  return dropLast(1)
-}
 
 fun Int.toByteArray(): ByteArray = ByteBuffer.allocate(4).putInt(this).array()
 
