0d44dfbda2603b03f792cffcc7601f24173004e6
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2017-02-25 16:51:41 +0300

    Implement short int to bytes encoding

--- server/src/main/java/jetbrains/rsynk/extensions/DataFormatsExtensions.kt
+++ server/src/main/java/jetbrains/rsynk/extensions/DataFormatsExtensions.kt
@@ -18,10 +18,26 @@ import java.nio.ByteBuffer
 fun Int.toReversedByteArray(): ByteArray = ByteBuffer.allocate(4).putInt(this).array().reversedArray()
 
 
+/**
+ * Same as @{code Int.toReversedByteArray}
+ * Rsync sends reversed 4 bytes when it sends int.
+ * Received bytes should be reversed to reconstruct the sent integer.
+ * */
 fun ByteArray.reverseAndCastToInt(): Int {
   if (this.size != 4) {
     throw IllegalArgumentException("Cannot convert array of ${this.size} to Int")
   }
   return ByteBuffer.wrap(this.reversedArray()).int
+}
+
+/**
+ * Converts an integer to 2 byte array as follows:
+ * bytes[0] - lowest byte
+ * bytes[1] - second lowest byte
+ * */
+fun Int.twoLowestBytes(): ByteArray {
+  val b1 = this.toByte()
+  val b2 = this.ushr(8).toByte()
+  return byteArrayOf(b1, b2)
 }
 
