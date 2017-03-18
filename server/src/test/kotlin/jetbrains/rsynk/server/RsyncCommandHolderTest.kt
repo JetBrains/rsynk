0ecc395400a061b22089f255a858eaa7791d007c
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2017-03-18 19:54:08 +0300

    Test rsync --server --send command resolving

--- server/src/test/kotlin/jetbrains/rsynk/server/RsyncCommandHolderTest.kt
+++ server/src/test/kotlin/jetbrains/rsynk/server/RsyncCommandHolderTest.kt
@@ -1,31 +1,27 @@
 package jetbrains.rsynk.server
 
+import jetbrains.rsynk.command.AllCommandsResolver
 import jetbrains.rsynk.command.CommandNotFoundException
-import jetbrains.rsynk.command.RsyncCommandsResolver
-import jetbrains.rsynk.command.RsyncServerSendCommand
 import org.junit.Test
 
-class RsyncCommandHolderTest {
+class CommandsResolverTest {
 
-    private val rsyncCommandsHolder = RsyncCommandsResolver()
+    private val rsyncCommandsHolder = AllCommandsResolver()
 
     @Test(expected = CommandNotFoundException::class)
     fun empty_args_list_does_not_match_any_command() {
         rsyncCommandsHolder.resolve(emptyList())
     }
 
-    @Test(expected = CommandNotFoundException::class)
-    fun rsync_argument_should_be_in_first_place_to_resolve_command() {
-        val args = RsyncServerSendCommand().matchArgs
-        rsyncCommandsHolder.resolve(args.drop(1) + args.first())
-    }
-
     @Test
     fun can_resolve_rsync_server_send_command() {
-        val args = RsyncServerSendCommand().matchArgs
+        val args = listOf("rsync", "--server", "--sender")
         rsyncCommandsHolder.resolve(args)
-        rsyncCommandsHolder.resolve(args + listOf("e.GaAsd"))
-        rsyncCommandsHolder.resolve(args + listOf("--something-else"))
-        rsyncCommandsHolder.resolve(args + listOf("aaa", "--bb", "foo", "Bar"))
+    }
+
+    @Test(expected = CommandNotFoundException::class)
+    fun do_not_resolve_server_send_command_for_client_in_daemon_mode() {
+        val args = listOf("rsync", "--server", "--sender", "--daemon")
+        rsyncCommandsHolder.resolve(args)
     }
 }
