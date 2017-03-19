c333916f21373d665971b92b82d6a335b34c5291
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2017-03-20 00:05:09 +0300

    Add request data parser tests

---
+++ server/src/test/kotlin/jetbrains/rsynk/server/RsyncRequestDataParserTest.kt
@@ -0,0 +1,55 @@
+package jetbrains.rsynk.server
+
+import jetbrains.rsynk.command.RsyncRequestDataParser
+import jetbrains.rsynk.options.Option
+import org.junit.Assert
+import org.junit.Test
+
+class RsyncRequestDataParserTest {
+
+    @Test
+    fun parse_long_named_options_test() {
+        val data = RsyncRequestDataParser.parse(listOf("rsync", "--server", "--sender", "-LCd"))
+        Assert.assertTrue(data.options.server)
+        Assert.assertTrue(data.options.sender)
+    }
+
+    @Test
+    fun parse_short_named_options_test() {
+        val data = RsyncRequestDataParser.parse(listOf("rsync", "--server", "--sender", "-LCd"))
+        Assert.assertTrue(data.options.symlinkTimeSettings)
+        Assert.assertTrue(data.options.checksumSeedOrderFix)
+        Assert.assertEquals(Option.FileSelection.TransferDirectoriesWithoutContent, data.options.directoryMode)
+    }
+
+    @Test
+    fun parse_files_test() {
+        val data = RsyncRequestDataParser.parse(listOf("rsync", "--server", "--sender", "-Ld", "/path/to/firs/file",
+                "path/to/second-file"))
+        Assert.assertEquals(data.files.joinToString(), 2, data.files.size)
+        Assert.assertTrue(data.files.contains("/path/to/firs/file"))
+        Assert.assertTrue(data.files.contains("path/to/second-file"))
+    }
+
+    @Test
+    fun default_file_selection_status_test() {
+        val data = RsyncRequestDataParser.parse(listOf("rsync"))
+        Assert.assertEquals(Option.FileSelection.NoDirectories, data.options.directoryMode)
+    }
+
+    @Test
+    fun parse_pre_release_info_test() {
+        val data = RsyncRequestDataParser.parse(listOf("rsync", "--server", "--sender", "-Le31.100002C"))
+        Assert.assertEquals("31.100002", data.options.preReleaseInfo)
+    }
+
+    @Test
+    fun parse_non_rsync_command_test() {
+        try {
+            RsyncRequestDataParser.parse(listOf("--server", "-e.sd"))
+        } catch (t: Throwable) {
+            return
+        }
+        Assert.fail()
+    }
+}
