7e2eed7990439f762d4f7489ee719612a682c195
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2017-03-19 22:50:16 +0300

    Rename

--- server/src/test/kotlin/jetbrains/rsynk/server/CommandsResolverTest.kt
+++ server/src/test/kotlin/jetbrains/rsynk/server/CommandsResolverTest.kt
@@ -9,18 +9,18 @@ class CommandsResolverTest {
     private val rsyncCommandsHolder = AllCommandsResolver()
 
     @Test(expected = CommandNotFoundException::class)
-    fun empty_args_list_does_not_match_any_command() {
+    fun empty_args_list_does_not_match_any_command_test() {
         rsyncCommandsHolder.resolve(emptyList())
     }
 
     @Test
-    fun can_resolve_rsync_server_send_command() {
+    fun can_resolve_rsync_server_send_command_test() {
         val args = listOf("rsync", "--server", "--sender")
         rsyncCommandsHolder.resolve(args)
     }
 
     @Test(expected = CommandNotFoundException::class)
-    fun do_not_resolve_server_send_command_for_client_in_daemon_mode() {
+    fun do_not_resolve_server_send_command_for_client_in_daemon_mode_test() {
         val args = listOf("rsync", "--server", "--sender", "--daemon")
         rsyncCommandsHolder.resolve(args)
     }
