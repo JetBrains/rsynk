88bc7eb64f6eeee566bce8b70b64a1344ed8d31e
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2017-04-15 19:20:00 +0300

    Add more rsync functionality to file-list, add tests

--- rsync/src/main/kotlin/jetbrains/rsynk/files/file-list.kt
+++ rsync/src/main/kotlin/jetbrains/rsynk/files/file-list.kt
@@ -13,27 +13,33 @@ data class FileListBlock(val rootDirectory: FileInfo?,
     }
 }
 
-class FileList(private val isRecursive: Boolean) {
+class FileListsBlocks(private val isRecursive: Boolean) {
 
     private val blocks = ArrayList<FileListBlock>()
     private val stubDirectories = TreeMap<Int, FileInfo>()
 
-    private var stubDirectoryIndex: Int = 0
+    private var nextStubDirIndex: Int = 0
     private var nextDirIndex: Int
+
+    val hasStubDirs: Boolean
+        get() = stubDirectories.isNotEmpty()
 
     init {
         nextDirIndex = if (isRecursive) 0 else -1
     }
 
-    fun addFileBlock(root: FileInfo?, fileList: List<FileInfo>): FileListBlock {
+    fun addFileBlock(root: FileInfo?,
+                     fileList: List<FileInfo>): FileListBlock {
 
         if (isRecursive) {
-            fileList.filter { it.isDirectory }.sorted().forEach { dir ->
-                if (dir.isNotDotDir) {
-                    stubDirectories[stubDirectoryIndex] = dir
-                }
-                stubDirectoryIndex++
-            }
+            fileList.filter { it.isDirectory }
+                    .sorted()
+                    .forEach { dir ->
+                        if (dir.isNotDotDir) {
+                            stubDirectories[nextStubDirIndex] = dir
+                        }
+                        nextStubDirIndex++
+                    }
         }
 
         val startIndex = nextDirIndex + 1 // preserve index for root (even if it's null)
@@ -41,11 +47,21 @@ class FileList(private val isRecursive: Boolean) {
 
         val map = TreeMap<Int, FileInfo>()
         val blockSize = (startIndex..lastIndex).zip(fileList.sorted()).toMap(map)        //TODO prune duplicates
-        val size = fileList.filter { it.isSymlink || it.isReqularFile }.foldRight(0, { file, sum: Long -> sum + file.size })
+        val size = fileList
+                .filter { it.isSymlink || it.isReqularFile }
+                .foldRight(0, { file, sum: Long -> sum + file.size })
         val block = FileListBlock(root, blockSize, startIndex, lastIndex, size)
         blocks.add(block)
 
         nextDirIndex = lastIndex + 1
         return block
+    }
+
+    fun getFileListBlocks(): List<FileListBlock> = blocks
+
+    fun popStubDir(index: Int): FileInfo? {
+        val foundDir = stubDirectories[index] ?: return null
+        stubDirectories.remove(index)
+        return foundDir
     }
 }
---
+++ rsync/src/test/kotlin/jetbrains/rsynk/FileListBlocksTest.kt
@@ -0,0 +1,87 @@
+package jetbrains.rsynk
+
+import jetbrains.rsynk.files.FileInfo
+import jetbrains.rsynk.files.FileListsBlocks
+import jetbrains.rsynk.files.Group
+import jetbrains.rsynk.files.User
+import org.junit.Assert
+import org.junit.Test
+import java.nio.file.Paths
+
+class FileListBlocksTest {
+
+    companion object Files {
+
+        private val user = User("nobody", 0)
+        private val group = Group("nobody", 0)
+        private val regularFileMode = 33416
+        private val directoryMode = 17161
+
+        val dotDirectory = FileInfo(Paths.get("."), directoryMode, 0, 0, User("nobody", 0), Group("nobody", 0))
+        val directory = FileInfo(Paths.get("haha"), directoryMode, 42, System.currentTimeMillis(), user, group)
+        val file = FileInfo(Paths.get("haha/hoho"), regularFileMode, 42, System.currentTimeMillis(), user, group)
+    }
+
+    @Test
+    fun preserve_index_for_not_null_root_non_recursive_test() {
+        val blocks = FileListsBlocks(false)
+        blocks.addFileBlock(Files.directory, listOf(file))
+        val block = blocks.getFileListBlocks().single()
+        Assert.assertEquals(Files.directory, block.rootDirectory)
+
+        val blockFiles = block.files
+        Assert.assertEquals(1, blockFiles.size)
+        Assert.assertEquals(Files.file, blockFiles[0])
+    }
+
+    @Test
+    fun preserve_index_for_null_root_non_recursive_test() {
+        val blocks = FileListsBlocks(false)
+        blocks.addFileBlock(null, listOf(file))
+        val block = blocks.getFileListBlocks().single()
+        Assert.assertNull(block.rootDirectory)
+
+        val blockFiles = block.files
+        Assert.assertEquals(1, blockFiles.size)
+        Assert.assertEquals(Files.file, blockFiles[0])
+    }
+
+    @Test
+    fun directory_index_is_minus_1_for_non_recursive_mode_test() {
+        val blocks = FileListsBlocks(false)
+        blocks.addFileBlock(null, listOf(Files.file))
+        val blockFiles = blocks.getFileListBlocks().single().files
+        Assert.assertEquals(1, blockFiles.size)
+        Assert.assertEquals(Files.file, blockFiles[0])
+    }
+
+    @Test
+    fun first_directory_index_is_zero_for_recursive_mode_test() {
+        val blocks = FileListsBlocks(true)
+        blocks.addFileBlock(null, listOf(Files.file))
+        val blockFiles = blocks.getFileListBlocks().single().files
+        Assert.assertEquals(1, blockFiles.size)
+        Assert.assertEquals(Files.file, blockFiles[1])
+    }
+
+    @Test
+    fun empty_stub_directories_on_non_recursive_mode_test() {
+        val blocks = FileListsBlocks(false)
+        blocks.addFileBlock(Files.directory, listOf(Files.file))
+        Assert.assertNull(blocks.popStubDir(0))
+    }
+
+    @Test
+    fun not_empty_stub_directories_on_recursive_mode_test() {
+        val blocks = FileListsBlocks(true)
+        blocks.addFileBlock(null, listOf(Files.directory, Files.file))
+        Assert.assertEquals(Files.directory, blocks.popStubDir(0))
+    }
+
+    @Test
+    fun do_not_include_dot_dir_in_stub_dirs_test() {
+        val blocks = FileListsBlocks(true)
+        blocks.addFileBlock(null, listOf(Files.dotDirectory, Files.file))
+        Assert.assertNull(blocks.popStubDir(0))
+    }
+}
