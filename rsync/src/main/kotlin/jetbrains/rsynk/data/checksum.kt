02bc14ff519b89fcf1486ff1d1be508b20f091ea
Vyacheslav Lukianov <lvo@intellij.net>
2017-05-21 21:18:21 +0300

    add apache 2.0 license

--- rsync/src/main/kotlin/jetbrains/rsynk/data/checksum.kt
+++ rsync/src/main/kotlin/jetbrains/rsynk/data/checksum.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 - 2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jetbrains.rsynk.data
 
 import java.security.MessageDigest
@@ -13,14 +28,14 @@ object RollingChecksum {
         for (i in 0..(length - 4) step 4) {
 
             s2 += 4 * (s1 + bytes[offset + i + 0]) +
-                  3 * bytes[offset + i + 1] +
-                  2 * bytes[offset + i + 2] +
-                  1 * bytes[offset + i + 3]
+                    3 * bytes[offset + i + 1] +
+                    2 * bytes[offset + i + 2] +
+                    1 * bytes[offset + i + 3]
 
             s1 += bytes[offset + i + 0] +
-                  bytes[offset + i + 1] +
-                  bytes[offset + i + 2] +
-                  bytes[offset + i + 3]
+                    bytes[offset + i + 1] +
+                    bytes[offset + i + 2] +
+                    bytes[offset + i + 3]
 
             index += 4
         }
