fbabef9d0c55a3b1217ac9eb3ae6a65c874b36ae
Sergey Voytovich <voytovich.sergeey@gmail.com>
2017-06-03 23:50:09 +0300

    fix setup envrionment

--- protocol-testing/setup-single-file-environment.py
+++ protocol-testing/setup-single-file-environment.py
@@ -3,15 +3,14 @@ import os
 
 
 def get_path(dir_path, file_name, create=False, backup=False):
-	if create:
-		file_path = "{0}/{1}".format(dir_path, file_name)
-	
+	file_path = "{0}/{1}".format(dir_path, file_name)
+	if create:	
 		if backup:
 			os.rename(file_path, "{0}.backup".format(file_path))
 	
 		with open(file_path, "w+"):
 			pass
-	return os.path.abspath("{0}/{1}".format(dir_path, file_name))
+	return os.path.abspath(file_path)
 
 
 parser = argparse.ArgumentParser(description="Create source file and print rsync comands with proper arguments" +
@@ -32,8 +31,8 @@ lorem_ipsum = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 from_file_name = "from.txt"
 to_file_name = "to.txt"
-sniffed_input_name = "sniffed.input"
-sniffed_output_name = "sniffed.output"
+sniffed_input_name = "sniffed.input.log"
+sniffed_output_name = "sniffed.output.log"
 
 create = args.no_create is None
 
@@ -51,13 +50,13 @@ if (create):
 
 	print("Info: all initial fiels have been created")
 
-
+print("")
 print("########################################################")
 print("")
 print("Run rsync: ")
 print("rsync --rsync-path=/usr/local/bin/rsync -v --protocol 31 -e "
-      "\"ssh -p 22 -o StrictHostKeyChecking=no\""
-      "localhost:{0} {1}/output.txt".format(from_file_path, to_file_path))
+      "\"ssh -p 22 -o StrictHostKeyChecking=no\" "
+      "localhost:{0} {1}".format(from_file_path, to_file_path))
 
 print("")
 print("That makes '{0}' content be transerred to file '{1}'".format(from_file_name, to_file_name))
@@ -70,8 +69,10 @@ print("rsync --server --sender -ve.LsfxC . "
 print("")
 print("Provide input by writing it at '{0}'".format(sniffed_input_name))
 print("Read output from '{0}'".format(sniffed_output_name))
-print("(all files (should be) located at '{0})".format(os.path.abspath(args.directory)))
+print("")
+print("(all files are located at '{0})".format(os.path.abspath(args.directory)))
 print("")
 print("########################################################")
+print("")
 
 
