56cefbaf8dd02290bf3f972a107c521bc3a9da7d
Sergey Voytovich <voytovich.sergeey@gmail.com>
2017-08-13 15:51:41 +0300

    Get rid of super default interface methods calls

--- server/src/main/kotlin/jetbrains/rsynk/server/SSHSessionFactory.kt
+++ server/src/main/kotlin/jetbrains/rsynk/server/SSHSessionFactory.kt
@@ -30,21 +30,18 @@ class SSHSessionFactory {
 
     fun createSessionListener() = object : SessionListener {
         override fun sessionCreated(session: Session?) {
-            super.sessionCreated(session)
             session ?: return
             val serverSession = session as ServerSession?
             logger.info { "SSH session created, client ip=${serverSession?.clientAddress}" }
         }
 
         override fun sessionEvent(session: Session?, event: SessionListener.Event?) {
-            super.sessionEvent(session, event)
             event ?: return
             val serverSession = session as ServerSession?
             logger.debug { "SSH session event=$event, client ip=${serverSession?.clientAddress}" }
         }
 
         override fun sessionException(session: Session?, t: Throwable?) {
-            super.sessionException(session, t)
             if (t != null) {
                 val serverSession = session as ServerSession?
                 logger.error("SSH session exception: ${t.message}, client ip=${serverSession?.clientAddress}", t)
@@ -52,7 +49,6 @@ class SSHSessionFactory {
         }
 
         override fun sessionClosed(session: Session?) {
-            super.sessionClosed(session)
             val serverSession = session as ServerSession?
             logger.info { "SSH session closed, client ip=${serverSession?.clientAddress}" }
         }
